bbf3f769683da8484cb104a0785fb6ec983cd107
Xlcos <98946854+Sebastiao00@users.noreply.github.com>
2023-04-07 16:52:46 +0100

    Add files via upload

---
+++ project/pages/games/memory_game.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <!-- Design by foolishdeveloper.com -->
+    <title> Rigister </title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="./../../css/games/memory/memory.css">
+    <link rel="stylesheet" href="./../../css/games/memory/reset.css">
+    <link rel="stylesheet" href="./../../css/games/memory.css">
+    
+    <script defer src="./../../javascript/games/memory/memory_game.js"></script>
+</head>
+<body>
+
+<main>
+    <header>
+      <span class="player"></span>
+      <span>Tempo: <span class="timer">00</span></span>
+    </header>
+
+    <div class="grid"></div>
+  </main>
+
+
+</body>
+</html>
+
