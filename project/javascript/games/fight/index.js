e571d2798d32229a0b2cade96f710ded091dd90f
Xlcos <98946854+Sebastiao00@users.noreply.github.com>
2023-04-16 17:13:02 +0100

    Add files via upload

---
+++ project/javascript/games/fight/index.js
@@ -0,0 +1,56 @@
+const canvas = document.getElementById('canvas');
+const c = canvas.getContext('2d');
+
+canvas.width = 1024;
+canvas.height = 576;
+
+c.fillRect(0, 0, canvas.width, canvas.height);
+
+class Sprite {
+    constructor({ position, velocity }) {
+        this.position = position;
+        this.velocity = this.velocity;
+    }
+
+    draw() {
+        c.fillStyle = 'red';
+        c.fillRect(this.position.x, this.position.y, 50, 150)
+    }
+
+    update () {
+        this.draw();
+        this.position.y += 10;
+    }
+}
+
+const player = new Sprite({
+    position: {
+        x: 0,
+        y: 0
+    },
+    velocity: {
+        x: 0,
+        y: 0    
+    }    
+})
+
+const enemy = new Sprite({
+    position: {
+        x: 400,
+        y: 100
+    },
+    velocity: {
+        x: 0,
+        y: 0
+    }
+})
+
+console.log(player);
+
+function animate() {
+    window.requestAnimationFrame(animate);
+    player.update();
+    enemy.updat();
+}
+
+animate();
